04ba023ae60d4d4dbc013130b0f72ef540f3db51
webkingdev1 <webking.dev@gmail.com>
2017-05-13 18:06:11 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define("DB_HOST", 'localhost');
 define("DB_USER", 'root');
 define("DB_PASSWORD", '');
-define("DB_NAME", 'testz11');
+define("DB_NAME", 'test_z11');
 define("PREF", 'z11_');
 define("TEMPLATE", 'template/default/');
 define("ACTIONS", 'actions/');
